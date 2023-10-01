5beba53483d1b507344e3f9c39539b15b88c9887
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2023-09-30 19:11:39 -0700

    Added Client

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,11 @@
 using Api.Database;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 builder.Services.AddDbContext<MatchngDbContext>(options =>
 {
@@ -19,6 +19,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
